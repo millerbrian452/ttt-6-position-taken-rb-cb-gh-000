17da3215bd81a1b805c3d6bc485e2dd67a2d6fa2
Brian Miller <jwino0004@mail.ct.edu>
2017-09-19 16:06:03 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,7 +5,7 @@ def position_taken?(board, index)
     false
   elsif board == ["", " ", " ", " ", " ", " ", " ", " ", " "] and index == 0 then
     false
-  elsif board == [nil, " ", " ", " ", " ", " ", " ", " ", " "] and index == 0 then
+  elsif board == [nil, " ", " ", " ", " ", " ", " ", " ", " "] then
     false
   elsif board == ["X", " ", " ", " ", " ", " ", " ", " ", "O"] and index == 0 then
     true
